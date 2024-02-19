9f4838faae901663af0fd9095f14bd0878b52b6c
AzamatG17 <128783946+AzamatG17@users.noreply.github.com>
2024-02-19 10:17:58 +0500

    update

--- DiyorMarket.MVC/Lesson11/Stores/Sales/ISaleDataStore.cs
+++ DiyorMarket.MVC/Lesson11/Stores/Sales/ISaleDataStore.cs
@@ -7,6 +7,7 @@ namespace Lesson11.Stores.Sales
     {
         public GetSaleResponse? GetSales(string? searchString, int? customerId, int pageNumber);
         public Sale? GetSale(int id);
+        public Stream GetExportFile();
         public Sale? CreateSale(Sale category);
         public Sale? UpdateSale(Sale category);
         public void DeleteSale(int id);
