194395eea7db1b2f943bfdb6e64ddcf756720332
AzamatG17 <128783946+AzamatG17@users.noreply.github.com>
2024-04-05 13:15:34 +0500

    Create Forgot, Reset Password Request
    Create ForgotRequest and ResetPaswwordRequest, add properties for User

---
+++ DiyorMarketApi/DiyorMarket/LoginModels/ResetPasswordRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace DiyorMarket.LoginModels
+{
+    public class ResetPasswordRequest
+    {
+        public string Login { get; set; }
+        public string ResetCode { get; set; }
+        public string newPassword { get; set; }
+    }
+}
