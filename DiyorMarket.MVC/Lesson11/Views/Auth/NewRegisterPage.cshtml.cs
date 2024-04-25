cce4ce269ad488781802714e8dbabf324f98aeca
Zikirillo <128654271+Zikirillo2002@users.noreply.github.com>
2024-04-25 14:40:25 +0500

    Upgrade register page and supplies page

---
+++ DiyorMarket.MVC/Lesson11/Views/Auth/NewRegisterPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Lesson11.Views.Auth
+{
+    public class NewRegisterPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
